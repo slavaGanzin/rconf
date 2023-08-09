a5234108dad24ef4d27440dfb4f57101238b6126
Slava Ganzin <slava.ganzin@gmail.com>
2023-08-09 21:08:08 +0400

    improve logs

--- front/Log.js
+++ front/Log.js
@@ -11,11 +11,14 @@ export const Log = () => {
           })
         }
 
-        return <div key={x.time} className='fadeIn logLine' onClick={x => x.currentTarget.toggleClass('expanded')}>
-          <summary><div className={'status-'+x.status}/> {x.ip || x.id} {x.service ? ` ${x.service}:` : ''}{x.message}</summary>
+        return <div key={x.time+x.status+x.service} className='fadeIn logLine' onClick={x => x.currentTarget.toggleClass('expanded')}>
+          <summary><div className={'status-'+x.status}/> {x.id || x.ip} {x.service ? ` ${x.service}:` : ''}{x.message}</summary>
           <div className='log fadeIn'>{
             values(mapObjIndexed((v,k) => {
               if (k == 'diff') return
+              if (k == 'stdout') return <pre>{v}</pre>
+              if (k == 'stderr') return <pre style={{color: 'red'}}>{v}</pre>
+              if (k == 'error') return <pre style={{color: 'red'}}>{v}</pre>
               return <><div>{k}</div><pre>{v}</pre></>
             }, x.json))
           }
